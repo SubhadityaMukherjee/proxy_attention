f68095d9d10d1de05e8f9f3f64283caa7b2dbce4
SubhadityaMukherjee <msubhaditya@gmail.com>
2022-10-17 20:55:43 +0200

    backup_still_struggling_with_paralle

--- src/config.py
+++ src/config.py
@@ -62,5 +62,48 @@ ds_config = {
         "change_subset_attention": 0.1,  # What % of data should be augmented with proxy attention
         "save_model_every_n_epoch": 3,
     },
+    "asl_test_proxy_train": {
+        "ds_path": "/media/hdd/Datasets/asl/asl_alphabet_train",
+        "ds_name": "asl",
+        "name_fn": fish_name_fn,
+        "image_size": 64,
+        "batch_size": 256,
+        "network": resnet18,
+        "pretrained": False,
+        "epoch_steps": [1, 1],  # n0 epochs -> augment -> n1 epochs ...
+        "enable_default_augments": False,
+        "enable_proxy_attention": True,
+        "change_subset_attention": 0.1,  # What % of data should be augmented with proxy attention
+        "save_model_every_n_epoch": 3,
+    },
+    "asl_test_no_proxy_train": {
+        "ds_path": "/media/hdd/Datasets/asl/asl_alphabet_train",
+        "ds_name": "asl",
+        "name_fn": fish_name_fn,
+        "image_size": 64,
+        "batch_size": 256,
+        "network": resnet18,
+        "pretrained": False,
+        "epoch_steps": [1, 1],  # n0 epochs -> augment -> n1 epochs ...
+        "enable_default_augments": False,
+        "enable_proxy_attention": False,
+        "change_subset_attention": 0.1,  # What % of data should be augmented with proxy attention
+        "save_model_every_n_epoch": 3,
+    },
+    "asl_test_proxy_train_50": {
+        "ds_path": "/media/hdd/Datasets/asl/asl_alphabet_train",
+        "ds_name": "asl",
+        "name_fn": fish_name_fn,
+        "image_size": 64,
+        "batch_size": 256,
+        "network": resnet18,
+        "pretrained": False,
+        "epoch_steps": [1, 1],  # n0 epochs -> augment -> n1 epochs ...
+        "enable_default_augments": False,
+        "enable_proxy_attention": True,
+        "change_subset_attention": 50,  # What % of data should be augmented with proxy attention
+        "save_model_every_n_epoch": 3,
+    },
+
 
 }
