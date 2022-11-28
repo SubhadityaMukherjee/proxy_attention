3443e6f407254e65f2e9710b8eac66e6d8ab8892
SubhadityaMukherjee <msubhaditya@gmail.com>
2022-11-28 19:38:05 +0100

    julia_size_wrong_I_guess

--- jax_src/vgg_cifar10.jl
+++ jax_src/vgg_cifar10.jl
@@ -9,8 +9,11 @@ using MLDatasets: CIFAR10
 using MLUtils: splitobs
 using ProgressMeter: @showprogress
 using ExplainableAI
-using HTTP, FileIO, ImageMagick  
+using HTTP, FileIO, ImageMagick
 using ImageShow
+using Colors
+using ColorTypes
+
 
 
 if CUDA.has_cuda()
@@ -21,22 +24,22 @@ end
 function get_processed_data(args)
     x, y = CIFAR10(:train)[:]
 
-    (train_x, train_y), (val_x, val_y) = splitobs((x, y), at=1-args.valsplit)
+    (train_x, train_y), (val_x, val_y) = splitobs((x, y), at=1 - args.valsplit)
 
     train_x = float(train_x)
     train_y = onehotbatch(train_y, 0:9)
     val_x = float(val_x)
     val_y = onehotbatch(val_y, 0:9)
-    
+
     return (train_x, train_y), (val_x, val_y)
 end
 
 function get_test_data()
     test_x, test_y = CIFAR10(:test)[:]
-   
+
     test_x = float(test_x)
     test_y = onehotbatch(test_y, 0:9)
-    
+
     return test_x, test_y
 end
 
@@ -45,22 +48,22 @@ function vgg16(nclasses::Int)
     Chain([
         Conv((3, 3), 3 => 64, relu, pad=(1, 1), stride=(1, 1)),
         Conv((3, 3), 64 => 64, relu, pad=(1, 1), stride=(1, 1)),
-        MaxPool((2,2)),
+        MaxPool((2, 2)),
         Conv((3, 3), 64 => 128, relu, pad=(1, 1), stride=(1, 1)),
         Conv((3, 3), 128 => 128, relu, pad=(1, 1), stride=(1, 1)),
-        MaxPool((2,2)),
+        MaxPool((2, 2)),
         Conv((3, 3), 128 => 256, relu, pad=(1, 1), stride=(1, 1)),
         Conv((3, 3), 256 => 256, relu, pad=(1, 1), stride=(1, 1)),
         Conv((3, 3), 256 => 256, relu, pad=(1, 1), stride=(1, 1)),
-        MaxPool((2,2)),
+        MaxPool((2, 2)),
         Conv((3, 3), 256 => 512, relu, pad=(1, 1), stride=(1, 1)),
         Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
         Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
-        MaxPool((2,2)),
+        MaxPool((2, 2)),
         Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
         Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
         Conv((3, 3), 512 => 512, relu, pad=(1, 1), stride=(1, 1)),
-        MaxPool((2,2)),
+        MaxPool((2, 2)),
         flatten,
         Dense(512, 4096, relu),
         Dense(4096, 4096, relu),
@@ -79,14 +82,14 @@ end
 function train(; kws...)
     # Initialize the hyperparameters
     args = Args(; kws...)
-	
+
     # Load the train, validation data 
     train_data, val_data = get_processed_data(args)
-    
+
     train_loader = DataLoader(train_data, batchsize=args.batchsize, shuffle=true)
     val_loader = DataLoader(val_data, batchsize=args.batchsize)
 
-    @info("Constructing Model")	
+    @info("Constructing Model")
     m = vgg16(args.nclasses) |> gpu
 
     loss(x, y) = logitcrossentropy(m(x), y)
@@ -103,11 +106,11 @@ function train(; kws...)
 
         @showprogress for (x, y) in train_loader
             x, y = x |> gpu, y |> gpu
-            gs = Flux.gradient(() -> loss(x,y), ps)
+            gs = Flux.gradient(() -> loss(x, y), ps)
             Flux.update!(opt, ps, gs)
         end
 
-        validation_loss = 0f0
+        validation_loss = 0.0f0
         @showprogress for (x, y) in val_loader
             x, y = x |> gpu, y |> gpu
             validation_loss += loss(x, y)
@@ -153,13 +156,13 @@ function center_crop_view(im::AbstractMatrix, outsize=IMGSIZE)
     h2, w2 = div.(outsize, 2) # half height, half width of view
     h_adjust, w_adjust = adjust.(outsize)
     return @view im[
-        ((div(end, 2) - h2):(div(end, 2) + h2 - h_adjust)) .+ 1,
-        ((div(end, 2) - w2):(div(end, 2) + w2 - w_adjust)) .+ 1,
+        ((div(end, 2)-h2):(div(end, 2)+h2-h_adjust)).+1,
+        ((div(end, 2)-w2):(div(end, 2)+w2-w_adjust)).+1,
     ]
 end
 
 function preprocess_resize(im::AbstractMatrix{<:AbstractRGB}, T=Float32::Type{<:Real})
-    im =  center_crop_view(im)
+    im = center_crop_view(im)
     # im = (channelview(im) .- PYTORCH_MEAN) ./ PYTORCH_STD
     im = channelview(im)
     return convert.(T, PermutedDimsArray(im, (3, 2, 1))) # Convert Image.jl's CHW to WHC
@@ -171,78 +174,38 @@ load_and_resize(image) = reshape(preprocess_resize(load(image)), 224, 224, 3, :)
 
 # load_and_resize(image) = reshape(load(image), 224, 224, 3, :)
 
-function extend_dims(A,which_dim)
-       s = [size(A)...]
-       insert!(s,which_dim,1)
-       return reshape(A, s...)
-       end
+function extend_dims(A, which_dim)
+    s = [size(A)...]
+    insert!(s, which_dim, 1)
+    return reshape(A, s...)
+end
 
 cd("/media/hdd/github/improving_robotics_datasets/jax_src/")
-ims_all = readdir("./data_testing/",join= true )
+ims_all = readdir("./data_testing/", join=true)
 
-ims = load_and_resize.(ims_all)
 
-model = convert_model(m[1:end-5]);
+model = convert_model(m[1:end-6]);
 analyzer = LRP(model);
-# expl = heatmap(input, analyzer,)
 heatmap_with_analyzer(x) = heatmap(x, analyzer)
+ims = load_and_resize.(ims_all)
 heat = heatmap_with_analyzer.(ims)
-heat
-test_im = load_and_resize(ims_all[2])
-# Add proxy attention here. Not multiply but convert in image based on value.
-using Colors
-using ColorTypes
-test_heat = heat[1]
-test_heat = Float32.(Gray.(test_heat))
-test_heat = ifelse.(test_heat .> 0.8, 1.0, 0.0)
-# extend_dims(test_heat,)
+heat[1]
+# test_im = load_and_resize(ims_all[2])
 
-# test_im
+# # Add proxy attention here. Not multiply but convert in image based on value.
+# test_heat = heat[1];
+# test_heat = Gray.(test_heat);
+# test_heat = ifelse.(test_heat .> 0.8, 1.0, 0.0)
 
-# Gray.(test_heat)
-# size(heat[1])
-# size(test_im)
-im_converted = test_im .* test_heat
+# im_converted = test_im .* test_heat
 
-# temp_test = reduce(+,im_converted;dims = 4)[:,:,:]
-# temp_test
-# ExplainableAI.ColorSchemes.seismic(temp_test)
-# heatmap(temp_test)
-# RGB(im_converted)[:,:,1]
-# im_converted[:,:,1]
-im_converted
-# Matrix(temp_test)
-# RGB.(Float32.(temp_test)[:,:,1])
-# temp_test = convert.(Normed{UInt8,8}, temp_test)
-im_converted = permutedims(im_converted[:,:,:], (3,1, 2))
-colorview(RGB,im_converted)
-# temp_test[:,:,2]
-# summary(temp_test)
-# RGB.(im_converted)
-# im_converted
-#  RGB.(channelview(im_converted))[:,:, 1]
-# im = RGB.(Float32.(ifelse.(im_converted .< -1.0 , 0.0, 1.0)[:,:,:]))
-using Images
-# (im)
-# Gray.(im_converted[:,:,:])
-# im_converted[:,:,1]
+# im_converted = permutedims(im_converted[:, :, :], (3, 1, 2))
+# colorview(RGB, im_converted)
 
-# test_im[test_heat .>0.5]
-# im_converted = permutedims(im_converted, (3, 1, 2))
-
-# im_converted = extend_dims(im_converted, 1)
-
-im_converted = colorview(RGB, im_converted/ 255)
-im_converted = map(clamp01nan, im_converted)
-
-
-# im_converted = extend_dims(im_converted, 1)
-
-# im_converted = colorview(RGB, im_converted);
-
-save("est.png",im_converted[:,:,:]);
-
-# im_converted = extend_dims(im_converted, 4)
-
-
-# im_converted[:,:,:]
+function proxy_attention_gen(image, heatmap, threshold=0.8)
+    heatmap = ifelse.(Gray.(heatmap) .> threshold, 1.0, 0.0)
+    image = colorview(RGB,permutedims(image[:,:,:] , (3,1,2)))
+    return image .* heatmap
+end
+# SIZE IS WRONG PROBABLY AAAAA
+proxy_attention_gen(ims[4], heat[4])
