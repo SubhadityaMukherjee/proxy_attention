eea976178671bedf04316769f6e6758d807ee445
eragon <eragon@pop-os.localdomain>
2023-04-29 14:14:33 +0200

    figures

--- src/results/visualize_dataset.py
+++ src/results/visualize_dataset.py
@@ -4,49 +4,47 @@ import torchvision
 import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
+from tqdm import tqdm
 # import proxyattention 
 #%%
 # Set the directory path for the dataset
 
-main_ds_dir = "/run/media/eragon/HDD/Datasets/"
+main_ds_dir = "/home/eragon/Documents/Datasets/"
 dataset_info = {
     "asl": {
-        "path": Path(f"{main_ds_dir}asl/asl_alphabet_train/"),
-        # "name_fn": proxyattention.data_utils.get_parent_name,
-        "num_classes" : 29
+        "path": Path(f"{main_ds_dir}asl/asl_alphabet_train/asl_alphabet_train"),
+        "num_classes": 29,
     },
     "imagenette": {
-        "path": Path(f"{main_ds_dir}/imagenette2-320"),
-
-        "num_classes" : 10
-    },
-    "caltech256": {
-        "path": Path(f"{main_ds_dir}/caltech256/train"),
-        "num_classes" : 256
+        "path": Path(f"{main_ds_dir}/imagenette2-320/train"),
+        "num_classes": 10,
     },
     "tinyimagenet": {
         "path": Path(f"{main_ds_dir}/tiny-imagenet-200/train"),
-        "num_classes" : 200
+        "num_classes": 200,
     },
     "cifar100": {
         "path": Path(f"{main_ds_dir}/CIFAR-100/train"),
-        "num_classes" : 100
+        "num_classes": 100,
     },
     "dogs": {
-        "path": Path(f"{main_ds_dir}/dogs/images/Images"),
-        "num_classes" : 120
+        "path": Path(f"{main_ds_dir}/dogs/images"),
+        "num_classes": 120,
     },
     "caltech101": {
         "path": Path(f"{main_ds_dir}/caltech-101"),
-        "num_classes" : 101
-    }
-
-
+        "num_classes": 101,
+    },
+    "plantdisease": {
+        "path": Path(
+            f"{main_ds_dir}/plantdisease/Plant_leave_diseases_dataset_without_augmentation"
+        ),
+        "num_classes": 39,
+    },
 }
-
 #%%
-# for name in dataset_info.keys():
-for name in ["asl"]:
+# for name in tqdm(dataset_info.keys(), total = len(dataset_info.keys())):
+for name in ["plantdisease"]:
     ds_name = name
     print(ds_name)
     # break
@@ -98,12 +96,14 @@ for name in ["asl"]:
         axs[i].imshow(img)
         axs[i].axis('off')
         if ds_name == "asl":
-            # axs[i].set_title(class_names[labels[i]])
-            pass
+            axs[i].set_title(class_names[labels[i]])
+            # pass
+        if ds_name == "plantdisease":
+            axs[i].set_title(class_names[labels[i]][:10])
         else:
             axs[i].set_title(class_names[labels[i]].split("-")[0])
 
     # plt.show()
-    plt.savefig(f"{ds_name}.png")
+    plt.savefig(f"/home/eragon/Documents/Github/proxy_attention/writing/thesis/images/{ds_name}.pdf", transparent = True)
 
 # %%
